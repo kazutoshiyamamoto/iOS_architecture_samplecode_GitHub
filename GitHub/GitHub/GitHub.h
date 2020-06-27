59c3ad0f41063c4ddb98be6f07d3c18126c021b2
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-06-27 23:26:32 +0900

    初回コミット

---
+++ GitHub/GitHub/GitHub.h
@@ -0,0 +1,19 @@
+//
+//  GitHub.h
+//  GitHub
+//
+//  Created by home on 2020/06/27.
+//  Copyright © 2020 Swift-beginners. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GitHub.
+FOUNDATION_EXPORT double GitHubVersionNumber;
+
+//! Project version string for GitHub.
+FOUNDATION_EXPORT const unsigned char GitHubVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GitHub/PublicHeader.h>
+
+
