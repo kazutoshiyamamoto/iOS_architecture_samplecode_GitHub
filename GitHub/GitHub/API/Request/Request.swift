3aff9e8e74d0aa39f6c9d7754515704c647c2c00
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-07-01 22:22:16 +0900

    HttpMethodの列挙型を追加

---
+++ GitHub/GitHub/API/Request/Request.swift
@@ -0,0 +1,13 @@
+//
+//  Request.swift
+//  GitHub
+//
+//  Created by home on 2020/07/01.
+//  Copyright © 2020 Swift-beginners. All rights reserved.
+//
+
+import Foundation
+public enum HttpMethod: String {
+    case get = "GET"
+    case post = "POST"
+}
