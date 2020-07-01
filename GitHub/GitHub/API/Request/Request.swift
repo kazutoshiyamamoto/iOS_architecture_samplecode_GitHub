a402fd97ec6e59796cfbe307160a09d40c06d831
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-07-01 22:22:51 +0900

    Requestプロトコルを追加

--- GitHub/GitHub/API/Request/Request.swift
+++ GitHub/GitHub/API/Request/Request.swift
@@ -7,6 +7,16 @@
 //
 
 import Foundation
+
+public protocol Request {
+    associatedtype Response: Decodable
+    var baseURL: URL { get }
+    var method: HttpMethod { get }
+    var path: String { get }
+    var headerFields: [String: String] { get }
+    var queryParameters: [String: String]? { get }
+}
+
 public enum HttpMethod: String {
     case get = "GET"
     case post = "POST"
