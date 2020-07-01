97435e6559acbdd223b02af2cef8a9268404bd97
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-07-01 18:42:57 +0900

    プロパティの初期化

--- GitHub/GitHub/API/Session.swift
+++ GitHub/GitHub/API/Session.swift
@@ -16,11 +16,25 @@ public enum SessionError: Error {
     case failedToCreateURL(URLComponents)
 }
 
+public final class Session {
+    private let accessToken: () -> AccessToken?
+    private let additionalHeaderFields: () -> [String: String]?
+    private let session: URLSession
+    
+    public init(accessToken: @escaping () -> AccessToken? = { nil },
+                additionalHeaderFields: @escaping () -> [String: String]? = { nil },
+                session: URLSession = .shared) {
+        self.accessToken = accessToken
+        self.additionalHeaderFields = additionalHeaderFields
+        self.session = session
+    }
+}
+
 extension SessionError {
     public struct Message: Decodable {
         public let documentationURL: URL
         public let message: String
-
+        
         private enum CodingKeys: String, CodingKey {
             case documentationURL = "documentation_url"
             case message
